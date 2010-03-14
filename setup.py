24552558be3c8581602710c4a1f5582b5d38a3ee
miohtama <miohtama@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-14 09:51:25 +0000

    Added setup.py to keep some tools happy.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='collective.developermanual',
+      version=version,
+      description="Plone community maintained developer documentation in Sphinx format",
+      #long_description=open("README.txt").read() + "\n",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='mFabrik Research Oy',
+      author_email='info@mfabrik.com',
+      url='http://mfabrik.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      #namespace_packages=['gomobiletheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
