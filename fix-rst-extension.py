c664a60ef5ee568dcd554f92abc60966c33cabab
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-04-06 21:06:03 +0300

    Mass add .rst extension to old theme manual

--- fix-rst-extension.py
+++ fix-rst-extension.py
@@ -12,4 +12,4 @@ top = sys.argv[1]
 for root, dirs, files in os.walk(top, topdown=False):
     for name in files:
         if not "." in name:
-            os.remove(os.path.join(root, name), os.path.join(root, name + ".rst"))
+            os.rename(os.path.join(root, name), os.path.join(root, name + ".rst"))
