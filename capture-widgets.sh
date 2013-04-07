18881be46321d5d50715bba7c18736ca9a8383ab
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-04-07 03:23:08 +0300

    Polishing capture script

--- capture-widgets.sh
+++ capture-widgets.sh
@@ -3,7 +3,7 @@
 # Capture plone.app.widgets widgets demo page
 #
 
-URL="http://localhost:8080/Plone/@@widgets-demo"
+URL="http://localhost:9090/Plone/@@widgets-demo"
 
 TARGET_FILE=widgets.html
 
@@ -18,8 +18,8 @@ sleep 3
 TDIR=`mktemp -d`
 echo "Capturing page $URL to $TDIR"
 
-trap control_c "cd $CWD"
-cd $TDIR ; wget --quiet --no-directories --level=1 --recursive --convert-links "$URL"
+trap "cd $CWD" SIGINT
+cd $TDIR ; wget --no-directories --level=1 --recursive --convert-links "$URL"
 GET_STATUS=$?
 cd $CWD
 
@@ -27,14 +27,14 @@ cd $CWD
 bin/instance-widget-capture stop
 
 # man pages don't tell about wget exit codes
-if [[ $GET_STATUS != 0 ]] ; then
-    echo "Could not succesfully read $URL"
+if [[ $GET_STATUS != 8 ]] ; then
+    echo "Could not succesfully read $URL, wget exit status $GET_STATUS"
     exit 1
 fi
 
-cp -r $TDIR $TARGET
+cp -r $TDIR/* $TARGET
 
-rm $TDIR
+rm -rf $TDIR
 
 
 
